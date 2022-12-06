00aa71c5ba96a7f0d167a83a4858198201f650ee
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-12-06 11:17:31 -0300

    refatorando o projeto

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,7 +1,7 @@
 import { Negociacao } from "./negociacao";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
