cf8da0c769cc46a8ede7e006e86958fca66917a9
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-01-19 11:46:54 -0300

    iniciando o curso de webpack

--- app/dist/js/decorators/escape.js
+++ app/dist/js/decorators/escape.js
@@ -1,12 +1,14 @@
-export function escape(target, propertKey, descriptor) {
+export function escape(target, propertyKey, descriptor) {
     const metodoOriginal = descriptor.value;
     descriptor.value = function (...args) {
         let retorno = metodoOriginal.apply(this, args);
         if (typeof retorno === 'string') {
-            retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
+            console.log(`@escape em ação na classe 
+                ${this.constructor.name} para o método ${propertyKey} `);
+            retorno = retorno
+                .replace(/<script>[\s\S]*?<\/script>/, '');
         }
         return retorno;
     };
     return descriptor;
 }
-//# sourceMappingURL=escape.js.map
