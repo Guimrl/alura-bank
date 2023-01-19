a39ed897a23b2818d8f58c632d5f2fe965afe092
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-01-19 13:03:12 -0300

    configurando htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: "./app/dist/js/app.js",
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'app/dist')
-    }
-
-
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './app/dist/app.html',
+            filename: 'app.html'
+        })
+    ]
 };
